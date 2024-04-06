9587d0f48b0c29c0f980ff42b5f30e5f56dd9a03
HsuShweSinOo <hsushwesinoo2002@gmail.com>
2024-04-06 21:42:21 +0630

    increase by 5 handler and toggle button finish

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,23 +4,36 @@ import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const isShow = useSelector((state) => state.isShow);
   const increaseHandler = () => {
     dispatch({ type: "increase" });
   };
   const decreaseHandler = () => {
     dispatch({ type: "decrease" });
   };
+  const increaseBy5Handler = () => {
+    dispatch({ type: "increaseBy5", amount: 5 });
+  };
+  const toggleHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   return (
     <section className="col-6 offset-3 text-center p-5 mt-5 border border-1 border-black bg-info">
       <h3>Redux Counter</h3>
-      <h1>{counter}</h1>
+      {isShow && <h1>{counter}</h1>}
+
       <hr />
       <button className="btn btn-primary me-3" onClick={increaseHandler}>
         Increase
       </button>
-      <button className="btn btn-warning me-3">Increase By 5</button>
-      <button className="btn btn-danger" onClick={decreaseHandler}>
+      <button className="btn btn-warning me-3" onClick={increaseBy5Handler}>
+        Increase By 5
+      </button>
+      <button className="btn btn-danger me-3" onClick={decreaseHandler}>
         Decrease
+      </button>
+      <button className="btn btn-secondary" onClick={toggleHandler}>
+        Toggle Counter
       </button>
     </section>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import { legacy_createStore as createStore } from "redux";
-
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, isShow: true };
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increase") {
     return {
       counter: state.counter + 1,
+      isShow: state.isShow,
     };
   }
   if (action.type === "decrease") {
     return {
       counter: state.counter - 1,
+      isShow: state.isShow,
+    };
+  }
+  if (action.type === "increaseBy5") {
+    return {
+      counter: state.counter + action.amount,
+      isShow: state.isShow,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      isShow: !state.isShow,
+      counter: state.counter,
     };
   }
   return state;
